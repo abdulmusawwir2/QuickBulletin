b07748b16c30374699cd0fc9e2cba86b58cee916
Abdul Musawwir <abdulmusawwir611.com>
2024-02-18 00:06:00 -0500

    second commit

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
-console.log("this is news js");
-let apikey='fb7c12b18c4b40e28d2cbde68658f0b9';
-let source='news24';
-let newsAccordion = document.getElementById("newsAccordion");
+console.log("This is my index js file");
 
-//create and get request
-const xhr=new XMLHttpRequest();
-xhr.open('GET',`https://newsapi.org/v2/top-headlines?sources=${source}&apikey=${apikey}`,true);
+// Initialize the news api parameters
+let source = 'the-times-of-india';
+let apiKey = 'd093053d72bc40248998159804e0e67d'
 
-//when response is ready
-xhr.onload=function(){
-    if(this.status===200){
-        let json=JSON.parse(this.responseText);
-        //console.log(json);
+// Grab the news container
+let newsAccordion = document.getElementById('newsAccordion');
+
+// Create an ajax get request
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`, true);
+
+// What to do when response is ready
+xhr.onload = function () {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
         let articles = json.articles;
-        //console.log(articles); 
-        let newsHtml="";
-        articles.forEach(function(element,index){ 
-            //console.log(articles[news]);
+        console.log(articles);
+        let newsHtml = "";
+        articles.forEach(function(element, index) {
+            // console.log(element, index)
+            let news = `<div class="card">
+                            <div class="card-header" id="heading${index}">
+                                <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                    aria-expanded="false" aria-controls="collapse${index}">
+                                   <b>Breaking News ${index+1}:</b> ${element["title"]}
+                                </button>
+                                </h2>
+                            </div>
 
-            let news =`<div class="card">
-             <div class="card-header" id="heading${index}">
-                <h2 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
-                        aria-expanded="true" aria-controls="collapse${index}">
-                        <b class="text-danger">  Breaking News ${index+1}: </b>${element["title"]}</button>
-                </h2>
-            </div>
-
-            <div id="collapse${index}" class="collapse " aria-labelledby="heading${index}"
-                data-parent="#newsAccordion">
-                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank">Read more here</a></div>
-            </div>
-        </div>`
-        newsHtml+=news;
+                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
+                                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
+                            </div>
+                        </div>`;
+            newsHtml += news;
         });
         newsAccordion.innerHTML = newsHtml;
     }
-    else{
-        console.log("some error occured");
+    else {
+        console.log("Some error occured")
     }
 }
-xhr.send();
+
+xhr.send()
 
 
